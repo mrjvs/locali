a1cf72ea1122498c79ad1afba41295bc8294d71a
mrjvs <mistrjvs@gmail.com>
2024-05-27 22:49:17 +0200

    Email validation

--- apps/frontend/src/pages/auth/login.vue
+++ apps/frontend/src/pages/auth/login.vue
@@ -33,7 +33,7 @@ const form = useForm({
     password: '',
   }),
   schema: z.object({
-    email: z.string().min(1, 'Please enter your email'),
+    email: z.string().min(1, 'Please enter your email').email('Please enter a valid email'),
     password: z.string().min(1, 'Please enter your password'),
   })
 })
