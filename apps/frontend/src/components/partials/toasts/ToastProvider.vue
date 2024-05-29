b55e33e6faabb0cbf47833cb2086c53c32d297aa
mrjvs <mistrjvs@gmail.com>
2024-05-29 16:14:59 +0200

    Animate toasts

--- apps/frontend/src/components/partials/toasts/ToastProvider.vue
+++ apps/frontend/src/components/partials/toasts/ToastProvider.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="fixed bottom-4 inset-x-0 flex flex-col items-center">
-    <div v-for="t of toasts.all" :key="t.id" class="mt-4">
-      <LclTransition animation="slide-from-top" :show="t.state === 'showing'">
+  <div class="fixed bottom-4 inset-x-0 pointer-events-none flex flex-col items-center">
+    <LclTransition animation="slide-from-bottom-fade-out" group>
+      <div v-for="t of showingToasts" :key="t.id" class="mt-4 pointer-events-auto">
         <Toast :toast="t.toast" />
-      </LclTransition>
-    </div>
+      </div>
+    </LclTransition>
   </div>
 </template>
 
 <script setup lang="ts">
 const toasts = useToasts();
+
+const showingToasts = computed(() => toasts.all.filter(v=>v.state === 'showing'))
 </script>
--- apps/frontend/src/components/ui/LclTransition.vue
+++ apps/frontend/src/components/ui/LclTransition.vue
@@ -11,6 +11,18 @@
   >
     <slot />
   </TransitionChild>
+  <TransitionGroup
+    v-else-if="props.group"
+    :enter-active-class="classes.enter"
+    :enter-from-class="classes.enterFrom"
+    :enter-to-class="classes.enterTo"
+    :leave-active-class="classes.leave"
+    :leave-from-class="classes.leaveFrom"
+    :leave-to-class="classes.leaveTo"
+    :move-class="classes.move"
+  >
+    <slot />
+  </TransitionGroup>
   <TransitionRoot
     v-else
     :show="props.show"
@@ -34,12 +46,13 @@ export type AnimationClasses = {
   enterFrom?: string;
   enterTo?: string;
   entered?: string;
+  move?: string;
   leave?: string;
   leaveFrom?: string;
   leaveTo?: string;
 };
 
-export type Animation = "slide-from-top" | "none";
+export type Animation = "slide-from-top" | "slide-from-bottom-fade-out" | "none";
 const animations: Record<Animation, AnimationClasses> = {
   "slide-from-top": {
     enter: "transition-[transform,opacity] duration-100",
@@ -49,11 +62,21 @@ const animations: Record<Animation, AnimationClasses> = {
     leaveFrom: "opacity-100 translate-y-0",
     leaveTo: "opacity-0 -translate-y-2",
   },
-  "none": {},
+  "slide-from-bottom-fade-out": {
+    enter: "transition-[transform,opacity] duration-200",
+    enterFrom: "opacity-0 translate-y-8",
+    enterTo: "opacity-100 translate-y-0",
+    leave: "transition-[transform,opacity] origin-center duration-200",
+    leaveFrom: "opacity-100 scale-100",
+    leaveTo: "opacity-0 scale-90",
+    move: "transition-all duration-200"
+  },
+  none: {},
 };
 
 const props = defineProps<{
   child?: boolean;
+  group?: boolean;
   show?: boolean;
   animation: Animation;
 }>();
