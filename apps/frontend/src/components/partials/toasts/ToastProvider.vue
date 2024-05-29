2c6e65034296bc50740fccb1e064d2df3c947edf
mrjvs <mistrjvs@gmail.com>
2024-05-29 14:53:29 +0200

    Center toasts

--- apps/frontend/src/components/partials/toasts/ToastProvider.vue
+++ apps/frontend/src/components/partials/toasts/ToastProvider.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed bottom-4 right-4 flex flex-col">
+  <div class="fixed bottom-4 inset-x-0 flex flex-col items-center">
     <div v-for="t of toasts.all" :key="t.id" class="mt-4">
       <LclTransition animation="slide-from-top" :show="t.state === 'showing'">
         <Toast :toast="t.toast" />
