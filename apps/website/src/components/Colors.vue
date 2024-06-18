178df4a3a49eee0fc942c5e2fe7845d1efaf5a6e
mrjvs <mistrjvs@gmail.com>
2024-06-18 21:10:08 +0200

    Responsiveness

--- apps/website/src/components/Colors.vue
+++ apps/website/src/components/Colors.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div class="absolute left-[-83px] top-[55px] w-[339px] h-[339px] blur-[500px] bg-[#8CF69D]"></div>
-    <div class="absolute right-[-50px] top-[55px] w-[494px] h-[327px] blur-[500px] bg-[#2135A5]"></div>
+    <div class="absolute left-[-83px] opacity-50 sm:top-[55px] w-[339px] h-[339px] blur-[250px] bg-[#8CF69D] top-[-55px]"></div>
+    <div class="absolute hidden sm:block right-[-50px] opacity-50 top-[-55px] w-[494px] h-[327px] blur-[250px] bg-[#2135A5]"></div>
+    <div class="absolute hidden sm:block right-[-80px] opacity-50 top-[193px] w-[339px] h-[339px] blur-[160px] bg-[#EF8969]"></div>
   </div>
 </template>
--- apps/website/src/components/Nav.vue
+++ apps/website/src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <Container>
-    <div class="flex items-center gap-6 my-4">
+    <div class="flex items-center px-4 gap-6 my-4">
       <Logo />
       <NuxtLink to="/" class="text-neutral-c800">Home</NuxtLink>
       <NuxtLink to="https://github.com/mrjvs/locali" class="hover:text-neutral-c800">Github</NuxtLink>
